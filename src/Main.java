526f9e8163aa3419f34e29fb9d4ac512f4638c28
TaniaKrupnova <88766616+TaniaKrupnova@users.noreply.github.com>
2021-09-18 17:06:28 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
 public class Main {
 
     public static void main(String[] args){
-        boolean program = false;
+        
         int sumTicket = 500;
-
-       int accrual;
-        if(program) {
-             accrual = 20;
-        }else {
-             accrual = 1;
-        }
-        int mili = sumTicket/20 * accrual;
-        System.out.println("Количество миль:" + mili);
+        int milli = sumTicket/20;
+        System.out.println("Количество миль:" + milli);
     }
 }
